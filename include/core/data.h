121d9edfb51b10633fe59d67893e8d737e962e16
ksiric <144100868+KarloSiric@users.noreply.github.com>
2025-06-26 10:13:47 +0200

    Created the write data callback custom function that the libcurl will need This function is needed because otherwise the libcurl will output the data into the stdout and we dont want that we want to process the data and store it into our data that we can use later when parsing the data

--- include/core/data.h
+++ include/core/data.h
@@ -1,6 +1,7 @@
 #ifndef DATA_H
 #define DATA_H 
 
+#include <stddef.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
@@ -21,11 +22,14 @@ typedef struct {
     int size;
 } s_LocationList;
 
+typedef struct {
+    char *data;
+    size_t data_size;
+} s_API_Response;
+
 
 s_LocationList *create_location_list(size_t initial_list_size);
 bool parse_json_data(const char *json_data, s_LocationList *location_list_data);
-
-
-
+int fetch_osm_data(const char *url, char **json_response);
 
 #endif
--- src/api/osm_api.c
+++ src/api/osm_api.c
@@ -2,13 +2,15 @@
 * @Author: karlosiric
 * @Date:   2025-06-26 09:03:28
 * @Last Modified by:   karlosiric
-* @Last Modified time: 2025-06-26 09:43:30
+* @Last Modified time: 2025-06-26 10:12:40
 */
 
 #include <cjson/cJSON.h>
 #include <errno.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <string.h>
+#include <curl/curl.h>
 #include "../../include/core/data.h"
 
 s_LocationList *create_location_list(size_t initial_list_size) {
@@ -33,8 +35,59 @@ s_LocationList *create_location_list(size_t initial_list_size) {
     return location_list;
 }
 
-bool parse_json_data(const char *json_data, s_LocationList *location_list_data) {
+/* we need our own function that will handle the data 
+ * that is being fetched from the https curl request.
+*/
+
+/**
+ * @brief      Writes a data.
+ *
+ * @param      buffer  The buffer
+ * @param[in]  size    The size of each element in the JSON 
+ * @param[in]  nmemb   The nmemb - the number of members inside each element
+ * @param      userp   The userp - data taht will be stored our custom data
+ *
+ * @return     { we return the total amount of bytes the total size that is being returned }
+ */
+
+size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp) {
     
+    s_API_Response *response = (s_API_Response *)userp;
+    size_t total_size = size * nmemb;
+
+    response->data = realloc(response->data, response->data_size + total_size + 1); 
+    if (!response->data) {
+        perror("Memory allocation failed, couldnt reallocate memory!");
+        return (0);
+    }
+    memcpy(response->data + response->data_size, buffer, total_size);
+    response->data_size += total_size;
+    response->data[response->data_size] = '\0';
+
+    // we need to tell libcurl how many bytes of data we have processed
+    return total_size; 
+}
+
+
+
+int fetch_osm_data(const char *url, char **json_response) {
+        
+    s_API_Response *response = {0};
+    response->data = malloc(NULL);
+    response->data_size = 0;
+
+    CURL *handle = curl_easy_init();
+    curl_easy_setopt(handle, CURLOPT_URL, url);
+    curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_data);
+
+
+    
+
+
+
+
+
+
 }
 
 
